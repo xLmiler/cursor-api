ea937bbf61bbf174e3188477acadb465a754c6e4
xLmiler <55581190+xLmiler@users.noreply.github.com>
2024-12-27 12:39:06 +0800

    Add files via upload

---
+++ src/models.js
@@ -0,0 +1,21 @@
+const models = {
+    "cursor-small":"cursor-small",
+    "gpt-3.5-turbo":"gpt-3.5-turbo",
+    "gpt-4":"gpt-4",
+    "gpt-4-turbo-2024-04-09":"gpt-4-turbo-2024-04-09",
+    "gpt-4o":"gpt-4o",
+    "gpt-4o-128k":"gpt-4o-128k",
+    "gpt-4o-mini":"gpt-4o-mini",
+    "o1-mini":"o1-mini",
+    "o1-preview":"o1-preview",
+    "claude-3-opus":"claude-3-opus",
+    "claude-3.5-sonnet":"claude-3.5-sonnet",
+    "claude-3-haiku":"claude-3-haiku",
+    "claude-3-haiku-200k":"claude-3-haiku-200k",
+    "claude-3-5-sonnet-200k":"claude-3-5-sonnet-200k",
+    "claude-3-5-sonnet-20241022":"claude-3-5-sonnet-20241022",
+    "gemini-1.5-flash-500k":"gemini-1.5-flash-500k",
+    "gemini-exp-1206":"gemini-exp-1206"
+}
+
+module.exports = { models };
